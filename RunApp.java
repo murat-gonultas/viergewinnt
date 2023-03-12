2f88ed256802288ff289d743d5fab0823c9a266b
Murat_G <murat.gonultas.formal@gmail.com>
2023-03-12 23:49:52 +0100

    Description changed

--- RunApp.java
+++ RunApp.java
@@ -1,21 +1,20 @@
 package com.github.muratgonultas;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class RunApp {
 
 	public static void gameIntro(){
-		System.out.println("|-------------------------------------GAME INTRO----------------------------------------|");
-		System.out.println("|---------------------------------------------------------------------------------------|");
-
-		System.out.println("|	1.	The game starts when the first player throws his chip into the desired column.	|\n" +
-				"|	2.	Then the turn passes to the other player.										|\n" +
-				"|	3.	The X sign represents the chip of the first player								|\n" +
-				"|		and the O sign represents the chip of the second player.						|\n"+
-				"|	4.	Whichever player brings his 4 jeeps horizontally, vertically or diagonally wins.|");
-		System.out.println("|---------------------------------------------------------------------------------------|");
-		System.out.println("|------------------------------------GOOD LUCK------------------------------------------|");
+		System.out.println(
+"""
+|-------------------------------------GAME INTRO----------------------------------------|
+|---------------------------------------------------------------------------------------|
+|	1.	The game starts when the first player throws his chip into the desired column.	|
+|	2.	Then the turn passes to the other player.										|
+|	3.	The X sign represents the chip of the first player								|
+|		and the O sign represents the chip of the second player.						|
+|	4.	Whichever player brings his 4 jeeps horizontally, vertically or diagonally wins.|
+|---------------------------------------------------------------------------------------|
+|------------------------------------GOOD LUCK!-----------------------------------------|
+""");
 	}
 
 
